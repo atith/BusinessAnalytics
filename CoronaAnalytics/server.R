6af6fce89ec3f71d9adfa0380777f2ac7fcb81aa
Fabi611 <fabienboeckle@gmail.com>
2020-07-26 19:09:40 +0200

    Darstellung von Graphik geht

--- CoronaAnalytics/server.R
+++ CoronaAnalytics/server.R
@@ -12,147 +12,62 @@
 library(shiny)
 
 # load required packages
-require(ggplot2)
+if(!require(magrittr)) install.packages("magrittr", repos = "http://cran.us.r-project.org")
+if(!require(rvest)) install.packages("rvest", repos = "http://cran.us.r-project.org")
+if(!require(readxl)) install.packages("readxl", repos = "http://cran.us.r-project.org")
+if(!require(dplyr)) install.packages("dplyr", repos = "http://cran.us.r-project.org")
+if(!require(maps)) install.packages("maps", repos = "http://cran.us.r-project.org")
+if(!require(ggplot2)) install.packages("ggplot2", repos = "http://cran.us.r-project.org")
+if(!require(reshape2)) install.packages("reshape2", repos = "http://cran.us.r-project.org")
+if(!require(ggiraph)) install.packages("ggiraph", repos = "http://cran.us.r-project.org")
+if(!require(RColorBrewer)) install.packages("RColorBrewer", repos = "http://cran.us.r-project.org")
+if(!require(leaflet)) install.packages("leaflet", repos = "http://cran.us.r-project.org")
+if(!require(plotly)) install.packages("plotly", repos = "http://cran.us.r-project.org")
+if(!require(geojsonio)) install.packages("geojsonio", repos = "http://cran.us.r-project.org")
 
-# import data
-# hier müssen wir die Daten definieren die geladen werden sollen
-
-# Define server logic required to draw a histogram
+# Define server logic required to draw a worldmap
 
 shinyServer(function(input, output) {
-        
-        # covid tab 
-        output$clean_date_reactive <- renderText({
-            format(as.POSIXct(input$plot_date),"%d %B %Y")
-        })
-        
-        reactive_db = reactive({
-            cv_cases %>% filter(date == input$plot_date)
-            # reactive = cv_cases %>% filter(date == "2020-04-25")
-        })
-        
-        reactive_db_last24h = reactive({
-            cv_cases %>% filter(date == input$plot_date & new_cases>0)
-        })
-        
-        reactive_db_large = reactive({
-            large_countries = reactive_db() %>% filter(alpha3 %in% worldcountry$ADM0_A3)
-            #large_countries = reactive %>% filter(alpha3 %in% worldcountry$ADM0_A3)
-            worldcountry_subset = worldcountry[worldcountry$ADM0_A3 %in% large_countries$alpha3, ]
-            large_countries = large_countries[match(worldcountry_subset$ADM0_A3, large_countries$alpha3),]
-            large_countries
-        })
-        
-        reactive_db_large_last24h = reactive({
-            large_countries = reactive_db_last24h() %>% filter(alpha3 %in% worldcountry$ADM0_A3)
-            large_countries = large_countries[order(large_countries$alpha3),]
-            large_countries
-        })
-        
-        reactive_polygons = reactive({
-            worldcountry[worldcountry$ADM0_A3 %in% reactive_db_large()$alpha3, ]
-        })
-        
-        reactive_polygons_last24h = reactive({
-            worldcountry[worldcountry$ADM0_A3 %in% reactive_db_large_last24h()$alpha3, ]
-        })
-        
-        output$reactive_case_count <- renderText({
-            paste0(prettyNum(sum(reactive_db()$cases), big.mark=","), " cases")
-        })
-        
-        output$reactive_death_count <- renderText({
-            paste0(prettyNum(sum(reactive_db()$deaths), big.mark=","), " deaths")
-        })
-        
-        output$reactive_recovered_count <- renderText({
-            paste0(prettyNum(sum(reactive_db()$recovered), big.mark=","), " recovered")
-        })
-        
-        output$reactive_active_count <- renderText({
-            paste0(prettyNum(sum(reactive_db()$active_cases), big.mark=","), " active cases")
-        })
-        
-        output$reactive_case_count_China <- renderText({
-            paste0("Mainland China: ", prettyNum(sum(subset(reactive_db(), country=="Mainland China")$cases), big.mark=",")," (",
-                   prettyNum((cv_aggregated %>% filter(date == input$plot_date & region=="Mainland China"))$new, big.mark=",")," new)")
-        })
-        
-        output$reactive_case_count_row <- renderText({
-            paste0("Other: ", prettyNum(sum(subset(reactive_db(), country!="Mainland China")$cases), big.mark=",")," (",
-                   prettyNum((cv_aggregated %>% filter(date == input$plot_date & region=="Other"))$new, big.mark=",")," new)")
-        })
-        
-        output$reactive_country_count <- renderText({
-            paste0(nrow(subset(reactive_db(), country!="Diamond Princess Cruise Ship")), " countries/regions affected")
-        })
-        
-        output$reactive_new_cases_24h <- renderText({
-            paste0((cv_aggregated %>% filter(date == input$plot_date & region=="Global"))$new, " new in last 24h")
-        })
-        
-        output$weltkarte <- renderLeaflet({ 
-            basemap
-        })
-        
-        observeEvent(input$plot_date, {
-            leafletProxy("map") %>% 
-                clearMarkers() %>%
-                clearShapes() %>%
-                addPolygons(data = reactive_polygons(), stroke = FALSE, smoothFactor = 0.1, fillOpacity = 0.15, fillColor = ~cv_pal(reactive_db_large()$deathsper100k)) %>%
-                
-                addCircleMarkers(data = reactive_db_last24h(), lat = ~ latitude, lng = ~ longitude, weight = 1, radius = ~(new_cases)^(1/5), 
-                                 fillOpacity = 0.1, color = covid_col, group = "2019-COVID (new)",
-                                 label = sprintf("<strong>%s (past 24h)</strong><br/>Confirmed cases: %g<br/>Deaths: %d<br/>Recovered: %d<br/>Cases per 100,000: %g<br/>Deaths per 100,000: %g", reactive_db_last24h()$country, reactive_db_last24h()$new_cases, reactive_db_last24h()$new_deaths, reactive_db_last24h()$new_recovered, reactive_db_last24h()$newper100k, reactive_db_last24h()$newdeathsper100k) %>% lapply(htmltools::HTML),
-                                 labelOptions = labelOptions(
-                                     style = list("font-weight" = "normal", padding = "3px 8px", "color" = covid_col),
-                                     textsize = "15px", direction = "auto")) %>%
-                
-                addCircleMarkers(data = reactive_db(), lat = ~ latitude, lng = ~ longitude, weight = 1, radius = ~(cases)^(1/5), 
-                                 fillOpacity = 0.1, color = covid_col, group = "2019-COVID (cumulative)",
-                                 label = sprintf("<strong>%s (cumulative)</strong><br/>Confirmed cases: %g<br/>Deaths: %d<br/>Recovered: %d<br/>Cases per 100,000: %g<br/>Deaths per 100,000: %g", reactive_db()$country, reactive_db()$cases, reactive_db()$deaths,reactive_db()$recovered, reactive_db()$per100k, reactive_db()$deathsper100k) %>% lapply(htmltools::HTML),
-                                 labelOptions = labelOptions(
-                                     style = list("font-weight" = "normal", padding = "3px 8px", "color" = covid_col),
-                                     textsize = "15px", direction = "auto")) %>%
-                
-                addCircleMarkers(data = reactive_db(), lat = ~ latitude, lng = ~ longitude, weight = 1, radius = ~(active_cases)^(1/5), 
-                                 fillOpacity = 0.1, color = covid_col, group = "2019-COVID (active)",
-                                 label = sprintf("<strong>%s (active)</strong><br/>Confirmed cases: %g<br/>Cases per 100,000: %g<br/><i><small>Excludes individuals known to have<br/>recovered (%g) or died (%g).</small></i>", reactive_db()$country, reactive_db()$active_cases, reactive_db()$activeper100k, reactive_db()$recovered, reactive_db()$deaths) %>% lapply(htmltools::HTML),
-                                 labelOptions = labelOptions(
-                                     style = list("font-weight" = "normal", padding = "3px 8px", "color" = covid_col),
-                                     textsize = "15px", direction = "auto"))  %>%
-                
-                addCircleMarkers(data = sars_final, lat = ~ latitude, lng = ~ longitude, weight = 1, radius = ~(cases)^(1/4), 
-                                 fillOpacity = 0.2, color = sars_col, group = "2003-SARS",
-                                 label = sprintf("<strong>%s</strong><br/>SARS cases: %g<br/>Deaths: %d<br/>Cases per 100,000: %g", sars_final$country, sars_final$cases, sars_final$deaths, sars_final$per100k) %>% lapply(htmltools::HTML),
-                                 labelOptions = labelOptions(
-                                     style = list("font-weight" = "normal", padding = "3px 8px", "color" = sars_col),
-                                     textsize = "15px", direction = "auto")) %>%
-                
-                addCircleMarkers(data = h1n1_cases, lat = ~ latitude, lng = ~ longitude, weight = 1, radius = ~(projected_deaths)^(1/4), 
-                                 fillOpacity = 0.2, color = h1n1_col, group = "2009-H1N1 (swine flu)",
-                                 label = sprintf("<strong>%s</strong><br/>H1N1 deaths (confirmed): %g<br/>H1N1 deaths (estimated): %g", h1n1_cases$region, h1n1_cases$deaths, h1n1_cases$projected_deaths) %>% lapply(htmltools::HTML),
-                                 labelOptions = labelOptions(
-                                     style = list("font-weight" = "normal", padding = "3px 8px", "color" = h1n1_col),
-                                     textsize = "15px", direction = "auto")) %>%
-                
-                addCircleMarkers(data = ebola_cases, lat = ~ latitude, lng = ~ longitude, weight = 1, radius = ~(cases)^(1/4), 
-                                 fillOpacity = 0.2, color = ebola_col, group = "2014-Ebola",
-                                 label = sprintf("<strong>%s</strong><br/>Ebola cases: %g<br/>Deaths: %d", ebola_cases$country, ebola_cases$cases, ebola_cases$deaths) %>% lapply(htmltools::HTML),
-                                 labelOptions = labelOptions(
-                                     style = list("font-weight" = "normal", padding = "3px 8px", "color" = ebola_col),
-                                     textsize = "15px", direction = "auto"))
-        })
-        
-        output$cumulative_plot <- renderPlot({
-            cumulative_plot(cv_aggregated, input$plot_date)
-        })
-        
-        output$epi_curve <- renderPlot({
-            new_cases_plot(cv_aggregated, input$plot_date)
-        })
-        
-
-    }
     
-)
+    # import data
+    # hier müssen wir die Daten definieren die geladen werden sollen
+    corona_cases = read.csv("input_data/coronacasesworlwide.csv")
+    laender = read.csv("input_data/countries_codes_and_coordinates.csv")
+    worldcountry = geojson_read("input_data/countries.geojson", what = "sp")
+    country_geoms = read.csv("input_data/country_geoms.csv")
+    
+    #daten müssen verarbeietet werden
+    
+    # extrahieren vom Datum der Corona Cases Tabelle
+    if (any(grepl("/", corona_cases$date))) { 
+        corona_cases$date = format(as.Date(corona_cases$date, format="%d/%m/%Y"),"%Y-%m-%d") 
+    } else { corona_cases$date = as.Date(corona_cases$date, format="%Y-%m-%d") }
+    corona_cases$date = as.Date(corona_cases$date)
+    cv_min_date = as.Date(min(corona_cases$date),"%Y-%m-%d")
+    current_date = as.Date(max(corona_cases$date),"%Y-%m-%d")
+    cv_max_date_clean = format(as.POSIXct(current_date),"%d %B %Y")   
+    
+    output$weltkarte<-renderLeaflet({
+        leaflet(worldcountry) %>%
+            addProviderTiles("CartoDB.Positron", options = providerTileOptions(noWrap = TRUE))
+    })
+    
+    url <- reactive({
+        glue("https://api.abalin.net/get/namedays?day={day_()}&month={month_()}")
+    })
+    
+    
+    #zum Testen der Panels
+    nv<-reactive(rnorm(input$ziehungen,
+                       input$erwartungswert,
+                       input$standardabweichung))
+    
+    output$Verteilung <- renderPlot({
+        
+        ggplot()+geom_histogram(aes(nv()), bins = 40)+
+            geom_vline(xintercept = mean(nv()), col="red", show.legend = TRUE)
+    })
+    
+    output$ari_mittel <- renderText(paste("Aritmethisches Mittel:", round(mean(nv()),2)))
+    
+})
--- CoronaAnalytics/ui.R
+++ CoronaAnalytics/ui.R
@@ -1,38 +1,23 @@
 library(shiny)
 
 #Install of package if required
-if (!require(magrittr))
-    install.packages("magrittr", repos = "http://cran.us.r-project.org")
-if (!require(rvest))
-    install.packages("rvest", repos = "http://cran.us.r-project.org")
-if (!require(readxl))
-    install.packages("readxl", repos = "http://cran.us.r-project.org")
-if (!require(dplyr))
-    install.packages("dplyr", repos = "http://cran.us.r-project.org")
-if (!require(maps))
-    install.packages("maps", repos = "http://cran.us.r-project.org")
-if (!require(ggplot2))
-    install.packages("ggplot2", repos = "http://cran.us.r-project.org")
-if (!require(reshape2))
-    install.packages("reshape2", repos = "http://cran.us.r-project.org")
-if (!require(ggiraph))
-    install.packages("ggiraph", repos = "http://cran.us.r-project.org")
-if (!require(RColorBrewer))
-    install.packages("RColorBrewer", repos = "http://cran.us.r-project.org")
-if (!require(leaflet))
-    install.packages("leaflet", repos = "http://cran.us.r-project.org")
-if (!require(plotly))
-    install.packages("plotly", repos = "http://cran.us.r-project.org")
-if (!require(geojsonio))
-    install.packages("geojsonio", repos = "http://cran.us.r-project.org")
-if (!require(shiny))
-    install.packages("shiny", repos = "http://cran.us.r-project.org")
-if (!require(shinyWidgets))
-    install.packages("shinyWidgets", repos = "http://cran.us.r-project.org")
-if (!require(shinydashboard))
-    install.packages("shinydashboard", repos = "http://cran.us.r-project.org")
-if (!require(shinythemes))
-    install.packages("shinythemes", repos = "http://cran.us.r-project.org")
+if(!require(magrittr)) install.packages("magrittr", repos = "http://cran.us.r-project.org")
+if(!require(rvest)) install.packages("rvest", repos = "http://cran.us.r-project.org")
+if(!require(readxl)) install.packages("readxl", repos = "http://cran.us.r-project.org")
+if(!require(dplyr)) install.packages("dplyr", repos = "http://cran.us.r-project.org")
+if(!require(maps)) install.packages("maps", repos = "http://cran.us.r-project.org")
+if(!require(ggplot2)) install.packages("ggplot2", repos = "http://cran.us.r-project.org")
+if(!require(reshape2)) install.packages("reshape2", repos = "http://cran.us.r-project.org")
+if(!require(ggiraph)) install.packages("ggiraph", repos = "http://cran.us.r-project.org")
+if(!require(RColorBrewer)) install.packages("RColorBrewer", repos = "http://cran.us.r-project.org")
+if(!require(leaflet)) install.packages("leaflet", repos = "http://cran.us.r-project.org")
+if(!require(plotly)) install.packages("plotly", repos = "http://cran.us.r-project.org")
+if(!require(geojsonio)) install.packages("geojsonio", repos = "http://cran.us.r-project.org")
+if (!require(shiny)) install.packages("shiny", repos = "http://cran.us.r-project.org")
+if (!require(shinyWidgets)) install.packages("shinyWidgets", repos = "http://cran.us.r-project.org")
+if (!require(shinydashboard)) install.packages("shinydashboard", repos = "http://cran.us.r-project.org")
+if (!require(shinythemes)) install.packages("shinythemes", repos = "http://cran.us.r-project.org")
+
 
 # Wir definieren eine 
 shinyUI(fluidPage(
@@ -46,7 +31,7 @@ shinyUI(fluidPage(
         "COVID-19 tracker",
         id = "nav",
         
-        #erste Seite wird Sars-Cov-2 und davon die die Ausbreitung weltweit zeigen
+        #1 Seite wird Sars-Cov-2 und davon die die Ausbreitung weltweit zeigen
         tabPanel(
             "COVID-19 Ausbreitung",
             div(
@@ -54,8 +39,7 @@ shinyUI(fluidPage(
                 tags$head(includeCSS("style.css")),
                 
                 #leafletOutput ist zur Darstellung einer Weltkarte
-                leafletOutput("weltkarte", width = "100%", height =
-                                  "100%"),
+                leafletOutput("weltkarte", width="400%", height ="400%"),
                 
                 absolutePanel(
                     id = "controls",
@@ -79,8 +63,8 @@ shinyUI(fluidPage(
                     )),
                     h6(textOutput("clean_date_reactive"), align = "right"),
                     h6(textOutput("reactive_country_count"), align = "right"),
-                    #plotOutput("epi_curve", height="130px", width="100%"),
-                    #plotOutput("cumulative_plot", height="130px", width="100%"),
+                    plotOutput("epi_curve", height="130px", width="100%"),
+                    plotOutput("cumulative_plot", height="130px", width="100%"),
                     
                     #Slider für das Datum
                     sliderInput(
@@ -98,7 +82,7 @@ shinyUI(fluidPage(
             )
         ),
         
-        #Darstellung der Weltwirtschaft 
+        #Seite 2 Darstellung der Weltwirtschaft 
         tabPanel(
             "Momentane Weltwirtschaft in den Ländern der Welt",
             div(
@@ -106,11 +90,11 @@ shinyUI(fluidPage(
                 tags$head(includeCSS("style.css")),
                 
                 #leafletOutput ist zur Darstellung einer Landkarte
-                leafletOutput("weltkarte", width =
+                leafletOutput("weltkarte2", width =
                                   "50%", height = "50%"),
                 
                 absolutePanel(
-                    id = "controls",
+                    id = "controls2",
                     class = "panel panel-default",
                     top = 75,
                     left = 55,
@@ -143,19 +127,19 @@ shinyUI(fluidPage(
             )     
         ),
         
-        #Panel für Zusammenhang von Covid19 und Weltwirtschaft
+        #Panel 3 für Zusammenhang von Covid19 und Weltwirtschaft
         tabPanel(
             "Korrelation zwischen Weltwirtschaft und Covid19",
             div(
                 class = "outer",
                 tags$head(includeCSS("style.css")),
                 
-                #leafletOutput ist zur Darstellung einer Landkarte
+                #leafletOutput ist zur Darstellung einer Landkarte - Es funktioniert nicht ich habe zwar ein Output Objekt
                 leafletOutput("landkarte", width =
                                   "50%", height = "50%"),
                 
                 absolutePanel(
-                    id = "controls",
+                    id = "controls3",
                     class = "panel panel-default",
                     top = 75,
                     left = 55,
@@ -169,21 +153,6 @@ shinyUI(fluidPage(
                             "Reported cases are subject to significant variation in testing policy and capacity between countries."
                         )
                     ), style = "color:#045a8d"),
-                    h3(textOutput("reactive_case_count"), align = "right"),
-                    h4(textOutput("reactive_death_count"), align = "right"),
-                    span(h4(
-                        textOutput("reactive_recovered_count"), align = "right"
-                    ), style = "color:#006d2c"),
-                    span(h4(
-                        textOutput("reactive_active_count"), align = "right"
-                    ), style = "color:#cc4c02"),
-                    h6(textOutput("clean_date_reactive"), align = "right"),
-                    h6(textOutput("reactive_country_count"), align = "right"),
-                    plotOutput("epi_curve", height =
-                                   "130px", width = "100%"),
-                    plotOutput("cumulative_plot", height =
-                                   "130px", width = "100%"),
-                    
                     sliderInput(
                         "plot_date",
                         label = h5("Select mapping date"),
@@ -195,6 +164,51 @@ shinyUI(fluidPage(
                         animate =
                             animationOptions(interval = 3000, loop = FALSE)
                     )
+                )
+            )        
+        ),
+        #Panel für Zusammenhang von Covid19 und Weltwirtschaft
+        tabPanel(
+            "TEST",
+            div(
+                class = "outer",
+                tags$head(includeCSS("style.css")),
+                
+                # Show a plot of the generated distribution
+                plotOutput("Verteilung", width =
+                               "100%", height = "100%"),
+                
+                # Sidebar with a slider input for number of bins
+                    
+                absolutePanel(
+                        id = "testcontrols",
+                        class = "panel panel-default",
+                        top = 75,
+                        left = 55,
+                        width = 250,
+                        fixed = TRUE,
+                        draggable = TRUE,
+                        height = "auto",
+                        
+                        
+                        sliderInput("erwartungswert",
+                                    "Erwartungswert:",
+                                    min = 1,
+                                    max = 50,
+                                    value = 30),
+                        sliderInput("ziehungen",
+                                    "Ziehungen:",
+                                    min = 1,
+                                    max = 10000,
+                                    value = 100,
+                                    step = 100),           
+                        sliderInput("standardabweichung",
+                                    "Standardabweichung:",
+                                    min = 1,
+                                    max = 10,
+                                    value = 1),
+                        
+                        textOutput("ari_mittel")
                 )
             )        
         )
