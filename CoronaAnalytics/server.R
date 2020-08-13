830c463a2b18f735682f7c3c1b52481ca7d389e5
Fabi611 <fabienboeckle@gmail.com>
2020-08-13 22:08:32 +0200

    Endlich tut sich was auf der Karte

--- CoronaAnalytics/server.R
+++ CoronaAnalytics/server.R
@@ -28,7 +28,10 @@ if(!require(leaflet)) install.packages("leaflet", repos = "http://cran.us.r-proj
 if(!require(plotly)) install.packages("plotly", repos = "http://cran.us.r-project.org")
 if(!require(geojsonio)) install.packages("geojsonio", repos = "http://cran.us.r-project.org")
 if(!require(readr)) install.packages("geojsonio", repos = "http://cran.us.r-project.org")
-# if(!require(devtools)) install.packages("devtools"), devtools::install_github("kassambara/ggpubr")
+if(!require(sp)) install.packages("sp")
+if(!require(raster)) install.packages("raster")
+
+
 
 # Define server logic required to draw a worldmap
 
@@ -39,7 +42,7 @@ shinyServer(function(input, output) {
     corona_cases = read.csv("input_data/coronavirus.csv", header=TRUE, sep= ",")
     laender = read.csv("input_data/countries_codes_and_coordinates.csv", header=TRUE, sep=",")
     worldcountry = geojson_read("input_data/countries.geojson", what = "sp")
-    country_geoms = read.csv("input_data/country_geoms.csv")
+    country_geoms = read.csv("input_data/countries_codes_and_coordinates.csv")
     economy = read_excel("input_data/GDP_World.xlsx")
     bip_daten <- read_excel("input_data/GDP.xls")
     
@@ -54,30 +57,25 @@ shinyServer(function(input, output) {
     
     names(cv_gdp)[3] <- "BIP"
     
-    View(cv_gdp)
-
-    #daten müssen verarbeietet werden
-
-    # extrahieren vom Datum der Corona Cases Tabelle
-    # if (any(grepl(corona_cases$date))) {
-    #     corona_cases$date = format(as.Date(corona_cases$date, format="%d/%m/%Y"),"%Y-%m-%d")
-    # } else { corona_cases$date = as.Date(corona_cases$date, format="%Y-%m-%d") }
-    # corona_cases$date = as.Date(corona_cases$date)
-    # cv_min_date = as.Date(min(corona_cases$date),"%Y-%m-%d")
-    # current_date = as.Date(max(corona_cases$date),"%Y-%m-%d")
-    # cv_max_date_clean = format(as.POSIXct(current_date),"%d %B %Y")
+    #Bip-Daten werden mit Standortdaten angereichert -> zum test nehme ich nur ein Jahr und zwar 2020
+    #bip_daten[2:nrow(bip_daten),5:ncol(bip_daten)] = lapply(bip_daten[2:nrow(bip_daten),5:ncol(bip_daten)], FUN = as.numeric)
     
+    bip_daten[ bip_daten == "no data" ] <- NA
+    bip_daten = subset(bip_daten, select=c("Country","2020"))
+    bip_daten["2020"] <- sapply(bip_daten["2020"], as.numeric)
+    round(bip_daten["2020"], 3)
+    bip_daten <- merge(bip_daten, country_geoms, by.x="Country", by.y="Country")
+    bip_daten$longitude <- as.numeric(bip_daten$longitude)
+    bip_daten$latitude <- as.numeric(bip_daten$latitude)
+
     
     #Karte für Covid 19
     meinemap=leaflet(worldcountry) %>% 
         addTiles() %>% 
-        addLayersControl(
-            position = "bottomright",
-            overlayGroups = c("2019-COVID (new)", "2019-COVID (active)", "2019-COVID (cumulative)"),
-            options = layersControlOptions(collapsed = FALSE)) %>% 
-        hideGroup(c("2019-COVID (active)", "2019-COVID (cumulative)"))  %>%
         addProviderTiles(providers$CartoDB.Positron) %>%
-        setView(0, 30, zoom = 2)
+        setView(0, 30, zoom = 2) %>%
+        addPolygons(stroke = FALSE, smoothFactor = 0.3, fillOpacity = 1,
+                  fillColor ="red")
     
     output$weltkarte<-renderLeaflet({
         meinemap
@@ -95,45 +93,56 @@ shinyServer(function(input, output) {
     output$Jahr <- renderText({ 
       paste("Ausgewähltes Jahr:", formating(bp()))
     })
-    
-    # Plotting Parameter kreieren für das BIP
-    #plot_year <- formating(bp())
-    #BIP_pal <- colorNumeric(palette = "Blues", domain = bip_daten$'2020')
         
     output$weltkarte2<-renderLeaflet({
       
-      bip_daten[ bip_daten == "no data" ] <- NA
+      
+      #keine Fehlerhaften daten und eindeutiges Datum
+
       plot_year <- formating(bp())
-      bip_daten[plot_year] = lapply(bip_daten[plot_year], FUN = as.numeric)
-      round(bip_daten[plot_year], 3)
-      #bip_daten[plot_year] = round(as.integer(bip_daten[plot_year]), digits = 0)
+      #Diese Funktion steht zum Test oben
+      #bip_daten["2020"] = lapply(bip_daten["2020"], FUN = as.numeric)
+     
+      
+      #wir müssen die Länder für die Karte selektieren
+      
+      bip_daten.SP <- SpatialPointsDataFrame(bip_daten[ ,c(7, 8)], bip_daten[,-c(7, 8)])
+      
+      View(bip_daten.SP)
+      
+      #coordinates(bip_daten) <- c("longitude", "latitude")
+      #crs.geo1 = CRS("+proj=longlat")  
+      #proj4string(bip_daten) = crs.geo1
+      #proj4string(worldcountry) = crs.geo1
+    
+      
+      #Hier definieren wir die Farben für die BIP Werte des Jahres
       bins <- c(0, 10, 20, 50, 100, 200, 500, 1000, Inf)
-      BIP_pal <- colorBin("YlOrRd", domain = bip_daten[plot_year], bins = bins)
-      
-      View(bip_daten[plot_year])
+      BIP_pal <- colorBin("YlOrRd", domain = bip_daten["2020"], bins = bins)
       
       
-     labels <- sprintf(
-        "<strong>%s</strong><br/>%g people / mi<sup>2</sup>",
-        bip_daten$Country, unlist(bip_daten[plot_year])
-      ) %>% lapply(htmltools::HTML)
+     #labels <- sprintf(
+      #  "<strong>%s</strong><br/>%g people / mi<sup>2</sup>",
+      #  bip_daten.SP$Country,
+      #) %>% lapply(htmltools::HTML)
       
       
-      leaflet(worldcountry) %>% 
+      leaflet() %>% 
         addTiles() %>% 
         addProviderTiles(providers$CartoDB.Positron) %>%
         setView(0, 30, zoom = 2) %>%
-        addPolygons(fillColor = ~BIP_pal, 
-                    weight = 2,
-                    opacity = 1,
-                    color = "white",
-                    dashArray = "3",
-                    fillOpacity = 0.7,
-                    label = labels,
-                    labelOptions = labelOptions(
-                      style = list("font-weight" = "normal", padding = "3px 8px"),
-                      textsize = "15px",
-                      direction = "auto"))
+        addMarkers(data = bip_daten)
+        #addPolygons(fillColor = "red", #~BIP_pal(plot_year), 
+        #            weight = 2,
+        #            opacity = 1,
+        #            color = "white",
+        #            dashArray = "3",
+        #            fillOpacity = 0.7,
+        #            label = labels,
+        #            labelOptions = labelOptions(
+        #              style = list("font-weight" = "normal", padding = "3px 8px"),
+        #              textsize = "15px",
+        #              direction = "auto"))
     })
     
     
@@ -174,20 +183,8 @@ shinyServer(function(input, output) {
         xlim (10, 500000) +
         geom_smooth(method = "lm")
 
-      # sp + stat_cor(aes(color = cyl), label.x = 3)
-      # #> `geom_smooth()` using formula 'y ~ x'
-      # 
-      # ggplot(cv_gdp, aes(displ, hwy)) +
-      #   geom_point() +
-      #   geom_smooth(method = "lm")
     })
     
-    # output$corona <- renderPlot({
-    #     
-    #     ggplot(data=corona_cases, aes(Date, Cases, group = 1)) + 
-    #         geom_line() +
-    #         labs(x = "Date", y = "Cases")
-    # })
     
     output$ari_mittel <- renderText(paste("Aritmethisches Mittel:", round(mean(nv()),2)))
     
